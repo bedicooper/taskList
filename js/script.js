5ac75072bb9dd7cbb8773921eed27d908b68e03a
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2022-12-04 21:39:44 +0100

    adding new tasks to array, displaying task buttons

--- js/script.js
+++ js/script.js
@@ -1,23 +1,38 @@
 {
     const tasks = [
-        { },
-];
+        ];
+
+const addNewTask = (newTaskContent) => {
+    tasks.push({
+        content: newTaskContent,
+        done: false,
+    });
+
+    render();
+};
 
 const render = () => {
     let htmlString = "";
 
     for (const task of tasks) {
         htmlString += `
-        <li>
-        ${task.content}
+        <li class="list__item${task.done ? " list__item--done" : ""}">
+        <button class="js-done list__button${task.done ? " list__button--done" : ""}"></button>
+        <span class="list__content">${task.content}</span>
+        <button class="js-delete list__button list__button--delete"></button>
         </li>
         `;
     }
 document.querySelector(".js-tasks").innerHTML = htmlString;
+console.log(tasks);
 };
 
 const onFormSubmit = (event) => {
     event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    addNewTask(newTaskContent);
 };
 
 const init = () => {
